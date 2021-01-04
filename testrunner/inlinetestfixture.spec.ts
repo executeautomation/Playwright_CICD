69f5543f953ddfd0d688ff95e23c2b662a6da12e
Karthik KK <karthikkk@Karthiks-MBP.hub>
2021-01-04 16:01:25 +1300

    Initial commit

---
+++ testrunner/inlinetestfixture.spec.ts
@@ -0,0 +1,14 @@
+import {it, test} from "./fixture"
+
+
+it.skip("run the basic test of neowin", async ({context}) => {
+    var page = await context.newPage();
+    await page.goto("https://www.neowin.net/");
+})
+
+
+it.skip("should be skipped for the firefox test", (test, {browserName}) => {
+    test.skip(browserName === "firefox", "Skip the test as it doesn't support isMobile option")
+}, async ({page}) => {
+    await page.goto("https://www.neowin.net/");
+});
